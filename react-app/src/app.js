b57c5879b49ab91458f67f9de8c2fb1157993641
Hovo4686 <131679116+Hovo4686@users.noreply.github.com>
2023-07-13 00:12:32 +0400

    create header

---
+++ react-app/src/app.js
@@ -0,0 +1,16 @@
+import Header from './components/header';
+// import CryptoCurrencyList from './pages/cryptoCurrencyList';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {/* <CryptoCurrencyList /> */}
+
+        </div>
+        
+       
+    )
+}
+
+export default App;
